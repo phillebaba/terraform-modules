6510a20b45d48aa41b6b0844f04fef85e0c62168
Philip Laine <philip.laine@gmail.com>
2019-04-02 22:38:07 +0200

    Refactor outputs from modules

--- modules/s3-static-website/outputs.tf
+++ modules/s3-static-website/outputs.tf
@@ -1,3 +1,7 @@
-output "s3_website_endpoint" {
+output "website_endpoint" {
   value = "${aws_s3_bucket.default.website_endpoint}"
+}
+
+output "bucket_name" {
+  value = "${aws_s3_bucket.default.id}"
 }
